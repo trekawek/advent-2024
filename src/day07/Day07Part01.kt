202deed1adec5e370fc5bca8b69c3873935b1618
Tomek Rękawek <trekawek@gmail.com>
2024-12-07 11:39:19 +0100

    Day 07.

---
+++ src/day07/Day07Part01.kt
@@ -0,0 +1,34 @@
+package day07
+
+fun main() {
+    println(parseInput().filter { (lhs, rhs) -> checkIfSolutionExist(lhs, rhs) }.map { (lhs, _) -> lhs }.sum())
+}
+
+internal fun parseInput() = generateSequence(::readlnOrNull).map {
+    it.split(":").run {
+        val lhs = this[0].trim().toLong()
+        val rhs = this[1].split(' ').map(String::trim).filterNot(String::isEmpty).map(String::toLong)
+        Pair(lhs, rhs)
+    }
+}
+
+private fun checkIfSolutionExist(result: Long, args: List<Long>): Boolean {
+    val operatorCount = args.size - 1
+    for (operatorBitmask in (0..<(1 shl operatorCount))) {
+        var r = args.first()
+        for (i in (1..<args.size)) {
+            if (operatorBitmask and (1 shl i - 1) == 0) {
+                r += args[i]
+            } else {
+                r *= args[i]
+            }
+            if (r > result) {
+                break
+            }
+        }
+        if (r == result) {
+            return true
+        }
+    }
+    return false
+}
---
+++ src/day07/Day07Part02.kt
@@ -0,0 +1,49 @@
+package day07
+
+fun main() {
+    println(parseInput().filter { (lhs, rhs) -> checkIfSolutionExist(lhs, rhs) }.map { (lhs, _) -> lhs }.sum())
+}
+
+private fun checkIfSolutionExist(result: Long, args: List<Long>): Boolean {
+    val operatorList = OperatorList(args.size - 1)
+    do {
+        var r = args.first()
+        for (i in (1..<args.size)) {
+            r = when (operatorList[i - 1]) {
+                Operator.PLUS -> r + args[i]
+                Operator.MULTI -> r * args[i]
+                Operator.CAT -> "$r${args[i]}".toLong()
+            }
+            if (r > result) {
+                break
+            }
+        }
+        if (r == result) {
+            return true
+        }
+    } while (operatorList.next())
+    return false
+}
+
+private enum class Operator {
+    PLUS, MULTI, CAT
+}
+
+private class OperatorList(length: Int) {
+    private val operators = MutableList(length) { Operator.PLUS }
+
+    operator fun get(i: Int): Operator {
+        return operators[i]
+    }
+
+    fun next(): Boolean {
+        for (i in operators.indices) {
+            val e = (operators[i].ordinal + 1) % 3
+            operators[i] = Operator.entries[e]
+            if (e != 0) {
+                return true
+            }
+        }
+        return false
+    }
+}
