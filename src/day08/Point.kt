2f52c72335df80e5949f3393dc8b704c994b221b
Tomek Rękawek <trekawek@gmail.com>
2024-12-08 17:08:45 +0100

    Day 08.

---
+++ src/day08/Point.kt
@@ -0,0 +1,10 @@
+package day08
+
+typealias Point = Pair<Int, Int>
+
+typealias Vector = Pair<Int, Int>
+
+operator fun Point.plus(v: Vector) = Point(first + v.first, second + v.second)
+operator fun Point.minus(p: Point) = Vector(first - p.first, second - p.second)
+operator fun Vector.times(i: Int) = Vector(first * i, second * i)
+operator fun Vector.unaryMinus() = Vector(-first, -second)
