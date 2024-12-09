edb120d9df0a709a715c98772c3fbf761d0f5380
Tomek Rękawek <trekawek@gmail.com>
2024-12-09 16:04:02 +0100

    Day 09.

---
+++ src/day09/Day09Part01.kt
@@ -0,0 +1,33 @@
+package day09
+
+fun main() {
+    println(parseMap(readln()).let(::compact).let(::checksum))
+}
+
+fun parseMap(mapLine: String) = mapLine.toList().flatMapIndexed { i, l ->
+    val length = l.toString().toInt()
+    if (i % 2 == 0) {
+        (0..<length).map { i / 2 }
+    } else {
+        (0..<length).map { null }
+    }
+}
+
+private fun compact(map: List<Int?>): List<Int> {
+    val result = map.toMutableList()
+
+    while (true) {
+        val firstNull = result.indexOfFirst { it == null }
+        val lastElement = result.indexOfLast { it != null }
+        if (firstNull < lastElement) {
+            result[firstNull] = result[lastElement]
+            result[lastElement] = null
+        } else {
+            break
+        }
+    }
+
+    return result.toList().filterNotNull()
+}
+
+fun checksum(map: List<Int?>) = map.mapIndexed { i, v -> (i * (v ?: 0)).toLong() }.sum()
