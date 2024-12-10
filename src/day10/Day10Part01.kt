d0915de340d9ca1cafb2492011f86b345521782d
Tomek Rękawek <trekawek@gmail.com>
2024-12-10 22:54:20 +0100

    Day 10 - small improvements in the getReachable9s.

--- src/day10/Day10Part01.kt
+++ src/day10/Day10Part01.kt
@@ -10,14 +10,14 @@ fun readField() = generateSequence(::readlnOrNull).map { row -> row.toList().map
 fun findTrailheads(field: List<List<Int>>) =
     field.flatMapIndexed { i, row -> row.mapIndexed { j, e -> if (e == 0) Pair(i, j) else null }.filterNotNull() }
 
-fun getReachable9s(field: List<List<Int>>, position: Position): List<Pair<Int, Int>> {
+fun getReachable9s(field: List<List<Int>>, position: Position): List<Position> {
     val current = field[position.first][position.second]
     if (current == 9) {
         return listOf(position)
     }
     return Direction.entries.flatMap { d ->
         val newPosition = position + d
-        if (newPosition.first < 0 || newPosition.second < 0 || newPosition.first >= field.size || newPosition.second >= field[0].size) {
+        if (!(newPosition.first in field.indices && newPosition.second in field[newPosition.first].indices)) {
             listOf()
         } else if (field[newPosition.first][newPosition.second] == current + 1) {
             getReachable9s(field, newPosition)
