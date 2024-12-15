a349e0c7789f2c82c2e017f6fd18f01f08e460a3
Tomek Rękawek <trekawek@gmail.com>
2024-12-15 16:45:26 +0100

    Day 15.

---
+++ src/day15/Day15Part01.kt
@@ -0,0 +1,46 @@
+package day15
+
+fun main() {
+    val field = Field.readField()
+    val movements = generateSequence(::readlnOrNull).flatMap(String::toList).map(Direction::charToDirection).toList()
+
+    var p = field.findInitialPosition()
+    for (m in movements) {
+        val q = p + m
+        if (!field.isValidPosition(q)) {
+            continue
+        }
+        val moved = when (val v = field[q]) {
+            'O' -> tryToMoveBox(field, q, m)
+            '#' -> false
+            '.' -> true
+            else -> throw IllegalArgumentException("Invalid value: $v")
+        }
+        if (moved) {
+            field[p] = '.'
+            field[q] = '@'
+            p = q
+        }
+    }
+
+    println(boxGps(field).sum())
+}
+
+private fun boxGps(field: Field) = field.map { pos, c -> if (c == 'O') pos.first + pos.second * 100 else null }.filterNotNull()
+
+private fun tryToMoveBox(field: Field, p: Pair<Int, Int>, m: Direction): Boolean {
+    var q = p
+    while (field[q] == 'O') {
+        q += m
+        if (!field.isValidPosition(q)) {
+            return false
+        }
+    }
+    return if (field[q] == '.') {
+        field[q] = 'O'
+        field[p] = '.'
+        true
+    } else {
+        false
+    }
+}
---
+++ src/day15/Direction.kt
@@ -0,0 +1,17 @@
+package day15
+
+enum class Direction(val offset: Pair<Int, Int>) {
+    UP(Pair(0, -1)), RIGHT(Pair(1, 0)), DOWN(Pair(0, 1)), LEFT(Pair(-1, 0));
+
+    companion object {
+        fun charToDirection(c: Char): Direction {
+            return when (c) {
+                '^' -> UP
+                '>' -> RIGHT
+                'v' -> DOWN
+                '<' -> LEFT
+                else -> throw IllegalArgumentException("Not a direction char: $c")
+            }
+        }
+    }
+}
