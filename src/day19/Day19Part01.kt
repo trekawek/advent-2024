0cd5071522e8e7d13eb241c4cfec15d10e741c71
Tomek Rękawek <trekawek@gmail.com>
2024-12-19 15:11:32 +0100

    Day 19.

---
+++ src/day19/Day19Part01.kt
@@ -0,0 +1,22 @@
+package day19
+
+fun main() {
+    val patterns = readln().split(',').map(String::trim)
+    val designs = generateSequence(::readlnOrNull).filter(String::isNotBlank)
+    println(designs.filter { isPossible(it, patterns) }.count())
+}
+
+fun isPossible(design: String, patterns: List<String>): Boolean {
+    if (design.isEmpty()) {
+        return true
+    }
+    for (p in patterns) {
+        if (design.startsWith(p)) {
+            if (isPossible(design.removePrefix(p), patterns)) {
+                return true
+            }
+        }
+    }
+    return false
+}
+
---
+++ src/day19/Day19Part02.kt
@@ -0,0 +1,29 @@
+package day19
+
+fun main() {
+    val patterns = readln().split(',').map(String::trim)
+    val designs = generateSequence(::readlnOrNull).filter(String::isNotBlank).toList()
+    println(designs.sumOf {
+        val res = countPossibleArrangements(it, patterns)
+        res
+    })
+}
+
+fun countPossibleArrangements(
+    design: String,
+    patterns: List<String>,
+    cache: MutableMap<String, Long> = mutableMapOf()
+): Long {
+    if (cache.contains(design)) {
+        return cache[design]!!
+    }
+    if (design.isEmpty()) {
+        return 1
+    }
+    val results =
+        patterns.filter { design.startsWith(it) }
+            .sumOf { countPossibleArrangements(design.removePrefix(it), patterns, cache) }
+    cache[design] = results
+    return results
+}
+
